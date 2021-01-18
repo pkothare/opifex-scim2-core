fd49c11f68f3959afc1491d510afd33be69a92fd
Pranav A. Kothare <pranavkothare@live.com>
2021-01-17 23:47:16 -0500

    Update Name.cs

--- src/Opifex.Scim2.Core/Name.cs
+++ src/Opifex.Scim2.Core/Name.cs
@@ -1,17 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Opifex.Scim2.Core
 {
     public class Name
     {
-        public string Formatted => string.Join(' ',
-                HonorificPrefix,
-                GivenName,
-                MiddleName,
-                FamilyName,
-                HonorificSuffix).Trim();
+        public Name(string givenName)
+            : this(givenName, null)
+        {
+
+        }
+
+        public Name(string givenName, string familyName)
+            : this(givenName, null, familyName)
+        {
+
+        }
+
+        public Name(string givenName, string middleName, string familyName)
+            : this(givenName, middleName, familyName, null)
+        {
+
+        }
+
+        public Name(string givenName,
+            string middleName,
+            string familyName,
+            string honorificSuffix)
+            : this(null, givenName, middleName, familyName, honorificSuffix)
+        {
+
+        }
+
+        public Name(string honorificPrefix,
+            string givenName,
+            string middleName,
+            string familyName,
+            string honorificSuffix)
+        {
+            HonorificPrefix = honorificPrefix;
+            GivenName = givenName;
+            MiddleName = middleName;
+            FamilyName = familyName;
+            HonorificSuffix = honorificSuffix;
+        }
+
 
         public string FamilyName { get; }
 
@@ -22,5 +54,36 @@ namespace Opifex.Scim2.Core
         public string HonorificPrefix { get; }
 
         public string HonorificSuffix { get; }
+
+        public string Formatted => string.Join(' ', FormattableComponents).Trim();
+
+        private IEnumerable<string> FormattableComponents
+        {
+            get
+            {
+                var items = new List<string>();
+                if (!string.IsNullOrWhiteSpace(HonorificPrefix))
+                {
+                    items.Add(HonorificPrefix);
+                }
+                if (!string.IsNullOrWhiteSpace(GivenName))
+                {
+                    items.Add(GivenName);
+                }
+                if (!string.IsNullOrWhiteSpace(MiddleName))
+                {
+                    items.Add(MiddleName);
+                }
+                if (!string.IsNullOrWhiteSpace(FamilyName))
+                {
+                    items.Add(FamilyName);
+                }
+                if (!string.IsNullOrWhiteSpace(HonorificSuffix))
+                {
+                    items.Add(HonorificSuffix);
+                }
+                return items;
+            }
+        }
     }
 }
