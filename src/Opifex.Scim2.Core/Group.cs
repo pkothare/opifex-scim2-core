2359011d81b59d97e463fb4e57a8488e43c593d6
Pranav A. Kothare <pranavkothare@live.com>
2021-01-18 23:54:16 -0500

    Add Group.cs (#6)

---
+++ src/Opifex.Scim2.Core/Group.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Opifex.Scim2.Core
+{
+    public class Group<TKey, T2Key> : Resource<TKey>
+        where TKey : IKey
+        where T2Key : IKey
+    {
+        public string DisplayName { get; }
+
+        public List<User<T2Key>> Members { get; }
+    }
+}
